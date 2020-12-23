d962ff578637fd44c434b4fdbe6f162c823568ac
Herman Stoud Platou <hsplatou@gmail.com>
2020-12-23 23:14:34 +0100

    mv and upt main file

--- src/main.py
+++ src/main.py
@@ -1,12 +1,42 @@
-from flask import Flask
+import os
+from flask import Flask, flash, request, redirect, url_for
 from swagger_ui import flask_api_doc
 
-app = Flask(__name__)
+UPLOAD_FOLDER = './upload'
+ALLOWED_EXTENSIONS = {'.xes'}
 
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+# HELPERS
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+# ENDPOINTS
 flask_api_doc(app, config_path='../swagger.yaml',
               url_prefix='/api/doc', title='API doc')
 
 
-@app.route('/api/')
-def hello_world():
-    return 'Hello, World!'
+# @app.route('/api')
+# redirect("/api/doc")
+
+
+@app.route('/api/discover', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+            flash('No selected file')
