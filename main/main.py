c443186e7d586117642650b63e0201dd75962250
Herman Stoud Platou <hsplatou@gmail.com>
2020-12-23 12:50:15 +0100

    add testdata from pm4py repo

--- main/main.py
+++ main/main.py
@@ -1,4 +1,16 @@
-import pm4py
-log = pm4py.read_xes('running-example.xes')
-net, initial_marking, final_marking = pm4py.discover_petri_net_inductive(log)
-pm4py.view_petri_net(net, initial_marking, final_marking, format="svg")
+import os
+# importer
+from pm4py.objects.log.importer.xes import importer as xes_importer
+# discovery algorithm
+from pm4py.algo.discovery.inductive import algorithm as inductive_miner
+# visualizer
+from pm4py.visualization.process_tree import visualizer as pt_visualizer
+
+filePath = os.path.join("tests", "input_data", "running-example.xes")
+
+log = xes_importer.apply(filePath)
+
+net, initial_marking, final_marking = inductive_miner.apply(log)
+
+
+# comment
