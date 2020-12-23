fa7140b6b2766cb0049baa9158a10782243e27b1
Herman Stoud Platou <hsplatou@gmail.com>
2020-12-23 19:00:18 +0100

    wip main

--- main/main.py
+++ main/main.py
@@ -6,8 +6,9 @@ from pm4py.objects.log.importer.xes import importer as xes_importer
 from pm4py.algo.discovery.inductive import algorithm as inductive_miner
 from pm4py.objects.conversion.process_tree import converter
 
-# import
 relativePath = os.path.join("test-data", "running-example.xes")
+
+# import
 log = xes_importer.apply(relativePath)
 
 # mine
@@ -18,3 +19,8 @@ bpmn_graph = converter.apply(tree, variant=converter.Variants.TO_BPMN)
 
 # export to valid BPMN model
 pm4py.write_bpmn(bpmn_graph, "export.bpmn", enable_layout=True)
+
+# move file to export dir
+current = os.path.abspath(".")
+os.rename(os.path.join(current, "export.bpmn"),
+          os.path.join(current, "main/export/export.bpmn"))
