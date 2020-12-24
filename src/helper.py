25c8741ed281423c08d5061af9cfc4a1c42e44ff
Herman Stoud Platou <hsplatou@gmail.com>
2020-12-24 15:30:30 +0100

    upt helper

---
+++ src/helper.py
@@ -0,0 +1,25 @@
+import os.path
+import os
+import glob
+
+
+def wipe_dir(pPath):
+    """[deletes all filed in dir]
+
+    Args:
+        path ([string]): [absolute path to dir]
+    """
+    files = glob.glob(os.path.join(pPath, "*"))
+    for f in files:
+        os.remove(f)
+
+
+def mv_file_to_export_dir(pCurrent, pFilename):
+    """[heplper that moves file from root to export dir]
+
+    Args:
+        pCurrent ([string]): [CWD]
+        pFilename ([string]): [filename with ext]
+    """
+    os.rename(os.path.join(pCurrent, pFilename),
+              os.path.join(pCurrent, "export", pFilename))
