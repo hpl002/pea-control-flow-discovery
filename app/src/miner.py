d5ee41abcfff1ed71e06411939ed16947f4c6870
Herman Stoud Platou <hsplatou@gmail.com>
2020-12-25 19:01:38 +0100

    upt miner

--- app/src/miner.py
+++ app/src/miner.py
@@ -1,5 +1,4 @@
 import os
-import pm4py
 import helper
 from pathlib import Path
 from pm4py.objects.log.importer.xes import importer as xes_importer
@@ -7,6 +6,8 @@ from pm4py.algo.discovery.inductive import algorithm as inductive_miner
 from pm4py.objects.conversion.process_tree import converter
 from pm4py.objects.process_tree.exporter import exporter as ptml_exporter
 from pm4py.objects.process_tree.importer import importer as ptml_importer
+from pm4py.write import write_bpmn
+
 CURRENT = os.getcwd()
 
 
@@ -25,5 +26,5 @@ def translate():
     tree = ptml_importer.apply(relativePath)
     bpmn_graph = converter.apply(tree, variant=converter.Variants.TO_BPMN)
     helper.wipe_dir(os.path.join(CURRENT, "export"))
-    pm4py.write_bpmn(bpmn_graph, "file.bpmn", enable_layout=True)
+    write_bpmn(bpmn_graph, "file.bpmn", enable_layout=True)
     helper.mv_file_to_export_dir(CURRENT, "file.bpmn")
