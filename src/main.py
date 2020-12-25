881643943ba091c96130155e635868f2bb6fd42a
Herman Stoud Platou <hsplatou@gmail.com>
2020-12-25 13:42:42 +0100

    fix return errors if uploaded file is wrong type, no filename, etc

--- src/main.py
+++ src/main.py
@@ -13,9 +13,18 @@ app = Flask(__name__)
 
 
 # HELPERS
-def allowed_file(filename):
+def allowed_file(filename, extentions):
+    """[checks filename for allowed extension]
+
+    Args:
+        filename ([string]): [filename]
+        extentions ([arr:string]): [arr of extensions, w/o dot]
+
+    Returns:
+        [type]: [boolean]
+    """
     return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+           filename.rsplit('.', 1)[1].lower() in extentions
 
 
 # ENDPOINTS
@@ -27,16 +36,15 @@ flask_api_doc(app, config_path='swagger.yaml',
 def upload_file():
     if request.method == 'POST':
         if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
+            return 400, "No file found"
         file = request.files['file']
         if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            ext = file.filename.rsplit('.', 1)[1].lower()
-            helper.wipe_dir(UPLOAD_DIR_PATH)
-            file.save(os.path.join(UPLOAD_DIR_PATH, "file"+"."+ext))
-            message = miner.mine()
-            export_fileName = helper.get_filename_from_dir(DOWNLOAD_DIR_PATH)
-            return send_file(os.path.join(DOWNLOAD_DIR_PATH, export_fileName), attachment_filename=export_fileName, mimetype="text/xml")
+            return 400, "No filename found"
+        if file and not allowed_file(file.filename, ["xes"]):
+            return "Incompatible filetype. Expected .xes", 422
+        ext = file.filename.rsplit('.', 1)[1].lower()
+        helper.wipe_dir(UPLOAD_DIR_PATH)
+        file.save(os.path.join(UPLOAD_DIR_PATH, "file"+"."+ext))
+        message = miner.mine()
+        export_fileName = helper.get_filename_from_dir(DOWNLOAD_DIR_PATH)
+        return send_file(os.path.join(DOWNLOAD_DIR_PATH, export_fileName), attachment_filename=export_fileName, mimetype="text/xml")
