903223512b54da93ff3d51faadf1e43414f603c5
Herman Stoud Platou <hsplatou@gmail.com>
2020-12-25 16:37:14 +0100

    fix translate endpoint

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ flask_api_doc(app, config_path='swagger.yaml',
 
 
 @app.route('/api/discover', methods=['POST'])
-def upload_file():
+def discover():
     if request.method == 'POST':
         if 'file' not in request.files:
             return 400, "No file found"
@@ -31,4 +31,22 @@ def upload_file():
         file.save(os.path.join(UPLOAD_DIR_PATH, "file"+"."+ext))
         message = miner.mine()
         export_fileName = helper.get_filename_from_dir(DOWNLOAD_DIR_PATH)
-        return send_file(os.path.join(DOWNLOAD_DIR_PATH, export_fileName), attachment_filename=export_fileName, mimetype="text/xml")
+        return send_file(os.path.join(DOWNLOAD_DIR_PATH, export_fileName), attachment_filename=export_fileName, mimetype="text/xml", as_attachment=True)
+
+
+@app.route('/api/translate', methods=['POST'])
+def translate():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return 400, "No file found"
+        file = request.files['file']
+        if file.filename == '':
+            return 400, "No filename found"
+        if file and not helper.allowed_file(file.filename, ["ptml"]):
+            return "Incompatible filetype. Expected .ptml", 422
+        ext = file.filename.rsplit('.', 1)[1].lower()
+        helper.wipe_dir(UPLOAD_DIR_PATH)
+        file.save(os.path.join(UPLOAD_DIR_PATH, "file"+"."+ext))
+        message = miner.translate()
+        export_fileName = helper.get_filename_from_dir(DOWNLOAD_DIR_PATH)
+        return send_file(os.path.join(DOWNLOAD_DIR_PATH, export_fileName), attachment_filename=export_fileName, mimetype="text/xml", as_attachment=True)
--- src/miner.py
+++ src/miner.py
@@ -1,8 +1,3 @@
-
-# read file from import directory
-# mine it and return result to export directory
-
-
 import os
 import pm4py
 import helper
@@ -11,14 +6,24 @@ from pm4py.objects.log.importer.xes import importer as xes_importer
 from pm4py.algo.discovery.inductive import algorithm as inductive_miner
 from pm4py.objects.conversion.process_tree import converter
 from pm4py.objects.process_tree.exporter import exporter as ptml_exporter
+from pm4py.objects.process_tree.importer import importer as ptml_importer
 CURRENT = os.getcwd()
 
 
 def mine():
     """[mines .xes to process model / process tree] """
-
     log = xes_importer.apply(os.path.join("upload", "file.xes"))
     tree = inductive_miner.apply_tree(log)
     helper.wipe_dir(os.path.join(CURRENT, "export"))
     ptml_exporter.apply(tree, "file.ptml")
     helper.mv_file_to_export_dir(CURRENT, "file.ptml")
+
+
+def translate():
+    """[translates process tree to BPMN 2.0 model] """
+    relativePath = os.path.join("upload", "file.ptml")
+    tree = ptml_importer.apply(relativePath)
+    bpmn_graph = converter.apply(tree, variant=converter.Variants.TO_BPMN)
+    helper.wipe_dir(os.path.join(CURRENT, "export"))
+    pm4py.write_bpmn(bpmn_graph, "file.bpmn", enable_layout=True)
+    helper.mv_file_to_export_dir(CURRENT, "file.bpmn")
