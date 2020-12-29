c370bb1dffd7388c87f0166f698c9cc91a64fb2c
Herman Stoud Platou <hsplatou@gmail.com>
2020-12-30 00:40:19 +0100

    some tidy + added a evaluate endpoint

--- app/src/main.py
+++ app/src/main.py
@@ -12,7 +12,8 @@ ALLOWED_EXTENSIONS = {"xes", "bpmn", "ptml"}
 app = Flask(__name__)
 
 # ENDPOINTS
-flask_api_doc(app, config_path="swagger.yaml", url_prefix="/api/doc", title="API doc")
+flask_api_doc(app, config_path="swagger.yaml",
+              url_prefix="/api/doc", title="API doc")
 
 
 @app.route("/api/discover", methods=["POST"])
@@ -28,7 +29,7 @@ def discover():
         ext = file.filename.rsplit(".", 1)[1].lower()
         helper.wipe_dir(UPLOAD_DIR_PATH)
         file.save(os.path.join(UPLOAD_DIR_PATH, "file" + "." + ext))
-        message = miner.mine()
+        miner.mine()
         export_fileName = helper.get_filename_from_dir(DOWNLOAD_DIR_PATH)
         return send_file(
             os.path.join(DOWNLOAD_DIR_PATH, export_fileName),
@@ -51,7 +52,30 @@ def translate():
         ext = file.filename.rsplit(".", 1)[1].lower()
         helper.wipe_dir(UPLOAD_DIR_PATH)
         file.save(os.path.join(UPLOAD_DIR_PATH, "file" + "." + ext))
-        message = miner.translate()
+        miner.translate()
+        export_fileName = helper.get_filename_from_dir(DOWNLOAD_DIR_PATH)
+        return send_file(
+            os.path.join(DOWNLOAD_DIR_PATH, export_fileName),
+            attachment_filename=export_fileName,
+            mimetype="text/xml",
+            as_attachment=True,
+        )
+
+
+@app.route("/api/evaluate", methods=["POST"])
+def translate():
+    if request.method == "POST":
+        if "file" not in request.files:
+            return 400, "No file found"
+        file = request.files["file"]
+        if file.filename == "":
+            return 400, "No filename found"
+        if file and not helper.allowed_file(file.filename, ["ptml"]):
+            return "Incompatible filetype. Expected .ptml", 422
+        ext = file.filename.rsplit(".", 1)[1].lower()
+        helper.wipe_dir(UPLOAD_DIR_PATH)
+        file.save(os.path.join(UPLOAD_DIR_PATH, "file" + "." + ext))
+        miner.translate()
         export_fileName = helper.get_filename_from_dir(DOWNLOAD_DIR_PATH)
         return send_file(
             os.path.join(DOWNLOAD_DIR_PATH, export_fileName),
